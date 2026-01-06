20e2229e67e2d9cd9daf42a38e66fefccea299d2
USER01\miyan <miyano.baske@icloud.com>
2026-01-06 23:26:07 +0900

    文字数カウント方式に変更

--- app/routes.py
+++ app/routes.py
@@ -4,5 +4,19 @@ main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
-    
-    return render_template('index.html')
+
+    text = ""
+    char_count = 0
+    byte_count = 0
+
+    if request.method == "POST":
+        text = request.form.get("text", "")
+        char_count = len(text)
+        byte_count = len(text.encode("utf-8"))
+
+    return render_template(
+        "index.html",
+        text=text,
+        char_count=char_count,
+        byte_count=byte_count
+    )
